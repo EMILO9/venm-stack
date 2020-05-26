d26323e5b067a207a2746eadc3ab64681db98d29
Emil Olsen <emilberolsen@gmail.com>
2020-05-26 20:14:09 +0200

    fix

--- server/src/app.js
+++ server/src/app.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 require('./routes')(app)
 
-sequelize.sync({force: true}) //{force: true}
+sequelize.sync() //{force: true}
 .then(() => {
     app.listen(config.port)
     console.log(`Server started on ${config.port}`)
